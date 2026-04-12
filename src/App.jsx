35fa3c57edc9aa8418ad926e82ee8593b5ce8db5
Ashan Dimantha <Ashan Dimantha>
2026-04-12 12:05:46 +0530

    remove: zod

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ import RHFForm from "./components/RHFForms/RHFForm";
 import WithValidations from "./components/RHFForms/WithValidations";
 import SignUpWithRHFAndZod from "./components/SignUpWithRHFAndZod";
 import UncontrolledForm from "./components/UncontrolledForms/UncontrolledForm";
-import ZodoForm from "./components/Zodo/ZodoForm";
 import "./index.css";
-import Test from "./Test";
 
 
 function App() {
@@ -20,7 +18,6 @@ function App() {
       {/* <RHFForm /> */}
       {/* <Test /> */}
       {/* <WithValidations /> */}
-      {/* <ZodoForm /> */}
       <SignUpWithRHFAndZod />
     </div>
   );
