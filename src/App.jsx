07ae002badbe09743eb31ea245f46fdbab61805a
Ashan Dimantha <Ashan Dimantha>
2026-04-19 12:13:25 +0530

    add: day 7 stage 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
 import "./index.css";
-import ZodoForm from "./components/Zodo/ZodoForm";
-import UserProfile from "./components/BasicStates/UserProfile";
-import PDUserProfile from "./components/PropDrilling/PDUserProfile";
 import Layout from "./components/UseContextAPI/Layout";
 import { UserProvider } from "./context/UserContext";
 import CartLayout from "./components/CartApp/CartLayout";
 import { CartProvider } from "./context/CartContext";
-import SignUpWithRHFAndZod from "./components/Zodo/SignUpWithRHFAndZod";
 
 function App() {
   return (
     <div>
-      {/* <ZodoForm /> */}
-      <SignUpWithRHFAndZod />
-      {/* <UserProfile /> */}
-      {/* <PDUserProfile /> */}
       {/* <UserProvider>
         <Layout />
       </UserProvider> */}
 
-      {/* <CartProvider>
+      <CartProvider>
         <CartLayout />
-      </CartProvider> */}
+      </CartProvider>
     </div>
   );
 }
